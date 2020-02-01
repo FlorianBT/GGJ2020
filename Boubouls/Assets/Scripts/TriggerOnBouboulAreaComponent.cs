b01b0f833ba9cfd57e66ee3140b4de721c8e8499
Florian BOEUF <boeuf.florian@gmail.com>
2020-02-01 22:44:11 +0100

    Created a trigger area for Boubouls

---
+++ Boubouls/Assets/Scripts/TriggerOnBouboulAreaComponent.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine.Events;
+
+[System.Serializable]
+public class TriggerOnBouboulAreaEvent : UnityEvent<BouboulAIComponent> { }
+public class TriggerOnBouboulAreaComponent
+            : TriggerAreaComponent<BouboulAIComponent, TriggerOnBouboulAreaEvent>
+{}
+
