a32f11c698a64b94b8abd5c6a22df6e8c8ca848b
Florian BOEUF <boeuf.florian@gmail.com>
2020-02-02 15:41:02 +0100

    Added scenes and changed  scene order

--- Boubouls/Assets/Scripts/GameManager.cs
+++ Boubouls/Assets/Scripts/GameManager.cs
@@ -6,7 +6,7 @@ using UnityEngine.Events;
 
 public class GameManager : MonoBehaviour
 {
-    private string[] m_Scenes = new string[] { "FirstScene", "Tibo", "Marie", "Florian", "Main" };
+    private string[] m_Scenes = new string[] { "FirstScene", "SecondScene", "Tibo", "Marie", "Final" };
 
     public int m_CurrentSceneIndex = 0;
     public CanvasGroup m_FaderCanvasGroup;
